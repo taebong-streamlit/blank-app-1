8f51828a80efb71b90884bb838e3d7a2bd0ccdf6
taebong-streamlit <taebong1@gmail.com>
2024-10-22 06:45:16 +0000

    title 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 duh_sfp 고온 발생현황 ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
